0ab05526412c3a30e8c11c7ed7d9b003770c952c
Andrew Opanasenko <andrew@your-gift.ru>
2019-06-22 14:44:24 +0300

    Add tasks for concat, minify, autoprefex and live reload for CSS, JS and HTML files.

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+const gulp         = require('gulp');
+const concat       = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS     = require('gulp-clean-css');
+const uglify       = require('gulp-uglify');
+const del          = require('del');
+const browserSync  = require('browser-sync').create();
+
+// CSS files to concat
+const cssFiles = ['./src/css/main.css', './src/css/media.css'];
+// JS files to concat
+const jsFiles = ['./src/js/lib.js', './src/js/main.js'];
+
+// Task for concat, minify and live-reloading CSS files
+gulp.task('styles', () => {
+    return gulp.src(cssFiles)
+        .pipe(concat('style.css'))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({
+            compability: 'ie8',
+            level: 2
+        }))
+        .pipe(gulp.dest('./build/css/'))
+        .pipe(browserSync.stream());
+});
+
+// Task for concat, minify and live-reloading JS files
+gulp.task('scripts', () => {
+    return gulp.src(jsFiles)
+    .pipe(concat('script.js'))
+    .pipe(uglify({
+        toplevel: true
+    }))
+    .pipe(gulp.dest('./build/js/'))
+    .pipe(browserSync.stream());
+});
+
+// Task for watching for CSS, JS and HTML files
+gulp.task('watch', () => {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch('./src/css/**/*.css', gulp.series('styles'));
+    gulp.watch('./src/js/**/*.js', gulp.series('scripts'));
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+// Task for delete files in ./src/* before building
+gulp.task('clean', () => {
+    return del(['build/*']);
+});
+
+// Final task for building and watching the project
+gulp.task('dev', gulp.series('clean', gulp.parallel('styles', 'scripts'), 'watch'));
+gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'scripts')));
+
+
+/**
+ TODO: 
+ - стрелочные функции
+ - компиляция sass
+ - решить вопрос с передачей названий файлов в таски
+ - загрузить на гитхаб 
+ FIXM
+ */
---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+console.log(multiple(5, 5));
+console.log(multiple(5, 10));
+console.log(multiple(5, 15));
